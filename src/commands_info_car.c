59d59d053c93d773aa8a941cb29b0f6d87aaa11e
Camilleri Nathan <nathan.camilleri@epitech.eu>
2018-04-12 15:31:23 +0100

    all command for simulation

---
+++ src/commands_info_car.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2017
+** commands_info_car.c
+** File description:
+** Need4Stek
+*/
+
+void	get_info_lidar(void)
+{
+	printf("GET_INFO_LIDAR\n");
+}
+
+void	get_current_speed(void)
+{
+	printf("GET_CURRENT_SPEED\n");
+}
+
+void	get_current_wheels(void)
+{
+	printf("GET_CURRENT_WHEELS\n");
+}
+
+void	get_car_speed_max(void)
+{
+	printf("GET_CAR_SPEED_MAX\n");
+}
+
+void	get_car_speed_min(void)
+{
+	printf("GET_CAR_SPEED_MIN\n");
+}
---
+++ src/commands_info_time_and_cycle.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2017
+** commands_info_time_and_cycle.c
+** File description:
+** Need4Stek
+*/
+
+void	get_info_simtime(void)
+{
+	printf("GET_INFO_SIMTIME\n");
+}
+
+int	cycle_wait(int nb_cycle)
+(
+	printf("CYCLE_WAIT:%d\n", nb_cycle);
+	return (SUCCES);
+)
